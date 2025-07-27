1552e51073f2df169964aad91dcea528d0b307db
jagan-238 <eragalajaganmohanreddy1160.com>
2025-07-27 17:33:22 +0530

    Recipe

---
+++ firebase.js
@@ -0,0 +1,13 @@
+// firebase.js
+const firebaseConfig = {
+  apiKey: "AIzaSyDWoalk1LmrdNdngG_jCA2wzDu0_rxR6Oc",
+  authDomain: "web-b43.firebaseapp.com",
+  databaseURL: "https://web-b43-default-rtdb.firebaseio.com",
+  projectId: "web-b43",
+  storageBucket: "web-b43.firebasestorage.app",
+  messagingSenderId: "1043699448922",
+  appId: "1:1043699448922:web:75aca8f58416f651e4ec63",
+};
+
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
---
+++ script/product.js
@@ -0,0 +1,27 @@
+// script/products.js
+const foodItems = [
+  {
+    id: 1,
+    name: "Margherita Pizza",
+    price: 250,
+    image: "https://i.imgur.com/U3vTGjX.png"
+  },
+  {
+    id: 2,
+    name: "Veg Burger",
+    price: 150,
+    image: "https://i.imgur.com/b9zDbyb.png"
+  },
+  {
+    id: 3,
+    name: "French Fries",
+    price: 100,
+    image: "https://i.imgur.com/NFAUnPb.png"
+  },
+  {
+    id: 4,
+    name: "Paneer Wrap",
+    price: 180,
+    image: "https://i.imgur.com/k5A7DoX.png"
+  }
+];
